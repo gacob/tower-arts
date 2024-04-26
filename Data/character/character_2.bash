4bb28238f357d3b48345b43c1236669a41968f2c
gacob <josemiguel_pilas@hotmail.com>
2024-04-26 10:24:43 +0200

    Character Template 2

---
+++ Data/character/character_2.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+### Personaje 2: El Perfeccionista
+character_template_1 () {
+    character_name_1="El Perfeccionista"
+    character_description_1="¿Nunca te has fijado en que todos los demonios tienen sus cuernos colocados de forma simétrica? ¿Quién crees que los diseñó?"
+    hp=100
+    atk_p=1
+    atk_m=10
+    def_p=5
+    def_m=5
+    speed=5
+    crit_r=2
+    crit_h=150
+}
+
+character_skillset_1() {
+    echo 
+}
