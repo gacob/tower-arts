8f5cfcbb1cca028cdb921ead77e3846729cf5811
Fulcrum <joelchicano70@gmail.com>
2024-05-01 16:54:05 +0200

    combat.bash update

--- Data/combat/combat.bash
+++ Data/combat/combat.bash
@@ -9,29 +9,27 @@ character_turn(){
     echo 2. Mochila
 
     while [[ $myturn_option -ne 1 || $myturn_option -ne 2 ]]; do
+        myturn_option=0
         read -r -p "¿Qué quieres hacer?" myturn_option
         case $myturn_option in
-            1)  myturn_option=0
-                while [[ $character_move -lt 1 || $character_move -gt 4 ]]; do
+            1)  while [[ $character_move -lt 1 || $character_move -gt 4 ]]; do
                     character_skillset_1
                     echo "$c_skill_1_name"
                     echo "$c_skill_2_name"
                     echo "$c_skill_3_name"
                     echo "$c_skill_4_name"
+                    character_move=0
                     read -r -p "¿Cúal será tu próximo movimiento...?" character_move
                     case $character_move in
-                        1)  character_move=0
-                            echo "$c_skill_1_text"
+                        1)  echo "$c_skill_1_text"
                             mob_current_hp=$((mob_current_hp - mob_skill_1))
                             ;;
 
-                        2)  character_move=0
-                            echo "$c_skill_2_text"
+                        2)  echo "$c_skill_2_text"
                             mob_current_hp=$((mob_current_hp - mob_skill_2))
                             ;;
 
-                        3)  character_move=0
-                            if [[ $c_skill_3_name = "Vacío" ]]; then
+                        3)  if [[ $c_skill_3_name = "Vacío" ]]; then
                                 echo No tienes ninguna habilidad aquí. Selecciona otra
                             else
                                 echo "$c_skill_3_text"
@@ -39,8 +37,7 @@ character_turn(){
                             fi
                             ;;
 
-                        4)  character_move=0
-                            if [[ $c_skill_4_name = "Vacío" ]]; then
+                        4)  if [[ $c_skill_4_name = "Vacío" ]]; then
                                 echo No tienes ninguna habilidad aquí. Selecciona otra
                             else
                                 echo "$c_skill_4_text"
@@ -48,15 +45,12 @@ character_turn(){
                             fi
                             ;;
 
-                        *)  character_move=0
-                            echo "Elige una opción válida"
+                        *)  echo "Elige una opción válida"
                             ;;
                     esac
                 done
-                ;;
 
-            2)  myturn_option=0
-                bag_info
+            2)  bag_info
                 ;;
 
             *)  echo "Elige una opción válida"
