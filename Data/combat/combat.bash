7b8f81bbe2d4e26e6b0553e08f1f51c2db572e4d
Fulcrum <joelchicano70@gmail.com>
2024-05-01 18:16:45 +0200

    Character_turn fix

--- Data/combat/combat.bash
+++ Data/combat/combat.bash
@@ -9,16 +9,18 @@ character_turn(){
     echo 2. Mochila
 
     myturn_option=0
-    read -r -p "¿Qué quieres hacer?" myturn_option
-    while [[ $myturn_option -ne 0 ]]; do
+    while [[ $myturn_option -eq 0 ]]; do
+        myturn_option=0
+        read -r -p "¿Qué quieres hacer?" myturn_option
         case $myturn_option in
-            1)  while [[ $character_move -lt 1 || $character_move -gt 4 ]]; do
+            1)  character_move=0
+                while [[ $character_move -ge 0 ]]; do
                     character_skillset_1
                     echo "$c_skill_1_name"
                     echo "$c_skill_2_name"
                     echo "$c_skill_3_name"
                     echo "$c_skill_4_name"
-                    character_move=0
+                    character_turn=0
                     read -r -p "¿Cúal será tu próximo movimiento...?" character_move
                     case $character_move in
                         1)  echo "$c_skill_1_text"
@@ -28,6 +30,7 @@ character_turn(){
 
                         2)  echo "$c_skill_2_text"
                             mob_current_hp=$((mob_current_hp - mob_skill_2))
+                            break 2
                             ;;
 
                         3)  if [[ $c_skill_3_name = "Vacío" ]]; then
@@ -35,6 +38,7 @@ character_turn(){
                             else
                                 echo "$c_skill_3_text"
                                 mob_current_hp=$((mob_current_hp - mob_skill_3))
+                                break 2
                             fi
                             ;;
 
@@ -43,6 +47,7 @@ character_turn(){
                             else
                                 echo "$c_skill_4_text"
                                 mob_current_hp=$((mob_current_hp - mob_skill_4))
+                                break 2
                             fi
                             ;;
 
@@ -53,7 +58,7 @@ character_turn(){
                 ;;
 
             2)  bag_info
-                myturn_option=0
+                character_turn
                 ;;
 
             *)  echo "Elige una opción válida"
@@ -62,7 +67,6 @@ character_turn(){
     done
 }
 
-character_turn
 
 mob_turn(){
     echo Turno del monstruo
