4da3c322db284c9d05660edcd316f505d25bd029
Fulcrum <joelchicano70@gmail.com>
2024-05-01 15:13:23 +0200

    Character_1 update

--- Data/character/character_1.bash
+++ Data/character/character_1.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+full_percent=100
+
 ### Personaje 1 (Por Defecto): El Narcisista
 character_template_1() {
     character_name_1="El Narcisista"
@@ -15,7 +17,17 @@ character_template_1() {
 }
 
 character_skillset_1() {
+    c_skill_1_name="Habilidad 1"
     c_skill_1=$(( (atk_p * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
     c_skill_1_text="$character_name usa [Golpe Básico]"
+    c_skill_2_name="Habilidad 2"
+    c_skill_2=$(( (atk_p * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
+    c_skill_2_text="$character_name usa [Golpe Duro]"
+    c_skill_3_name="Vacío"
+    c_skill_3=
+    c_skill_3_text="$character_name usa [Golpe Feo]"
+    c_skill_4_name="Vacío"
+    c_skill_4=
+    c_skill_4_text="$character_name usa [Golpe Celestial]"
     
 }
