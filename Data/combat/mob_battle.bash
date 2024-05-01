fd0a64f783d5494feae4572d73d83991407dd50f
Fulcrum <joelchicano70@gmail.com>
2024-05-01 15:13:59 +0200

    mob_battle.bash created. In progress

---
+++ Data/combat/mob_battle.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mob_battle(){
+
+    mob_current_hp=$mob_hp
+
+
+    if [[ $speed -ge $mob_speed ]]; then
+        while [[ $current_hp -ge 0 || $mob_count -gt 0 ]]; do
+            character_turn
+            mob_turn
+        done
+    else
+        while [[ $current_hp -ge 0 || $mob_count -gt 0 ]]; do
+            mob_turn
+            character_turn
+        done
+    fi
+}
