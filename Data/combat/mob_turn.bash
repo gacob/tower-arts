ff09eda2a00dd1f447fb53bb3eb981dc7240ac51
Fulcrum <joelchicano70@gmail.com>
2024-05-02 17:23:00 +0200

    Mob_turn update. To be fixed.

--- Data/combat/mob_turn.bash
+++ Data/combat/mob_turn.bash
@@ -2,4 +2,17 @@
 
 mob_turn(){
     echo Turno del monstruo
+
+    random_skill=$(( RANDOM % 4 + 1))
+    
+    mob_attack=mob_skill_text_${random_skill}
+    echo "${!mob_attack}"
+
+    mob_damage=mob_skill_${random_skill}
+
+    current_hp=$(( current_hp - mob_damage ))
+
+    echo "$current_hp"
+    
+    
 }
