c0804e2d4a4307188f1b148ab458f9e6df260e5f
Fulcrum <joelchicano70@gmail.com>
2024-05-02 20:17:54 +0200

    Fix to mob_turn.

--- Data/combat/mob_turn.bash
+++ Data/combat/mob_turn.bash
@@ -5,15 +5,13 @@ mob_turn(){
 
     random_skill=$(( RANDOM % 4 + 1))
     
-    mob_attack=mob_skill_text_${random_skill}
+    mob_attack="mob_skill_text_${random_skill}"
     echo "${!mob_attack}"
 
-    mob_damage=mob_skill_${random_skill}
-    echo "${!mob_damage}"
+    mob_damage="mob_skill_${random_skill}"
 
     current_hp=$(( current_hp - mob_damage ))
 
     echo "$current_hp"
-    
     
 }
