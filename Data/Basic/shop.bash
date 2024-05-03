a99c226a645bd7b30ea04d0aa454c75e4472ea40
fulcrum44 <115259065+joelchicano44@users.noreply.github.com>
2024-05-03 10:16:18 +0200

    Fix to shop.

--- Data/Basic/shop.bash
+++ Data/Basic/shop.bash
@@ -4,24 +4,6 @@ shop_option=(0-5)
 
 
 tienda() {
-    contador=1
-    drop_name=(
-    [0]="Joel Legendario"
-    [1]="Joel Épico"
-    [2]="Vela del Infierno"
-    [3]="Martillo Perdido de un Borracho"
-    [4]="Gafas con grado de Miopía"
-    [5]="Bastón de Andar de Dios"
-)
-
-drop_chance=(
-    [10]=0
-    [20]=1
-    [80]=2
-    [45]=3
-    [95]=4
-    [4]=5
-)
 
     # Seleccionamos 2 objetos para la tienda
     while [[ $contador -le 2 ]]; do
@@ -94,5 +76,3 @@ drop_chance=(
         esac
     done
 }
-
-tienda
