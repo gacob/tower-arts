0a4976ec8403222f8e8e3d419c30e966ddb46cc5
gacob <josemiguel_pilas@hotmail.com>
2024-05-23 12:01:39 +0200

    Habilidades de CH 1

--- Data/character/character_1.bash
+++ Data/character/character_1.bash
@@ -8,7 +8,7 @@ character_template_1() {
     character_description_1="El infierno sólo está caliente porque está él."
     hp_1=100
     atk_p_1=10
-    atk_m_1=1
+    atk_m_1=5
     def_p_1=5
     def_m_1=5
     speed_1=12
@@ -17,17 +17,17 @@ character_template_1() {
 character_skillset_1() {
     full_percent=100
 
-    c_1_skill_1_name="Habilidad 1"
-    c_1_skill_1=$(( (atk_p_1 * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
-    c_1_skill_1_text="usa [Golpe Básico]"
-    c_1_skill_2_name="Habilidad 2"
-    c_1_skill_2=$(( (atk_p_1 * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
-    c_1_skill_2_text="usa [Golpe Duro]"
-    c_1_skill_3_name="Vacío"
-    c_1_skill_3=$(( (atk_p_1 * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
-    c_1_skill_3_text="usa [Golpe Feo]"
-    c_1_skill_4_name="Vacío"
-    c_1_skill_4=$(( (atk_p_1 * 2 ) * ( full_percent - mob_def_p ) / "full_percent" ))
-    c_1_skill_4_text="usa [Golpe Celestial]"
+    c_1_skill_1_name="Detroit Smash [Físico]"
+    c_1_skill_1=$(( (atk_p_1 * 3 ) * ( full_percent - mob_def_p ) / "full_percent" ))
+    c_1_skill_1_text="usa [Detroit Smash]"
+    c_1_skill_2_name="Puño Incasdescente [Mágico]"
+    c_1_skill_2=$(( (atk_m_1 * 3 ) * ( full_percent - mob_def_p ) / "full_percent" ))
+    c_1_skill_2_text="usa [Puño Incasdescente]"
+    c_1_skill_3_name="Rugido de Titán de Ataque"
+    c_1_skill_3=$(( atk_p_1 + 5 ))
+    c_1_skill_3_text="usa [Rugido de Titán de Ataque]. ¡Su ataque físico ha subido!"
+    c_1_skill_4_name="Rugido de Titán Acorazado"
+    c_1_skill_4=$(( atk_p_1 + 2 && def_m_1 + 2 ))
+    c_1_skill_4_text="usa [Rugido de Titán Acorazado]. ¡Toda la defensa ha subido!"
     
 }
