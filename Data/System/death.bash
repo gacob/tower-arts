417f1c2ace5f73185902a0eb4f7e04e270a79d00
gacob <josemiguel_pilas@hotmail.com>
2024-05-23 19:25:06 +0200

    death fix?

--- Data/System/death.bash
+++ Data/System/death.bash
@@ -12,11 +12,7 @@ death() {
 
 
 dead() {
-    if [[ $current_hp_1 -le 0 ]]; then
+    if [[ $current_hp_1 -le 0 || $current_hp_2 -le 0 ]]; then
         dead=1
-    else 
-        if [[ $current_hp_2 -le 0 ]]; then
-        dead=1
-        fi
     fi
 }
