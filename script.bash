0faf35ac3a561778a0fb0f6366bbe7aef0a5aa24
Fulcrum <joelchicano70@gmail.com>
2024-05-23 22:01:36 +0200

    docker update

--- script.bash
+++ script.bash
@@ -2,15 +2,6 @@
 
 set -e
 
-cd home
-
-apt update
-apt install git
-git clone https://github.com/gacob/tower-arts.git
-
-cd tower-arts
-
-apt install -y dos2unix
 apt install -y sudo
 apt install -y iptables
 apt install -y nano
@@ -18,6 +9,7 @@ apt install -y net-tools
 apt install -y ncat
 apt install -y wget
 apt install -y bsdgames
+sudo iptables -I INPUT -i docker0 -j ACCEPT
 wget https://www.vpn.net/installers/logmein-hamachi-2.1.0.203-x64.tgz
 tar -xvzf logmein-hamachi-2.1.0.203-x64.tgz
 dos2unix *
