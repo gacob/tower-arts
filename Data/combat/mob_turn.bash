eda42790680f3d6020d394cc5a96d467b5c680c8
gacob <josemiguel_pilas@hotmail.com>
2024-05-23 12:40:41 +0200

    Shield

--- Data/combat/mob_turn.bash
+++ Data/combat/mob_turn.bash
@@ -3,7 +3,8 @@
 mob_turn(){
 
     random_skill=$(( RANDOM%4 + 1))
-    
+    random_target=$(RANDOM%2 + 1)
+
     mob_attack="mob_skill_text_${random_skill}"
     echo "$mob_name" usa "${!mob_attack}" > skill_text
 
@@ -13,7 +14,12 @@ mob_turn(){
 
         # Su tercer ataque es un Buff: Rugido
         if [[ $mob_damage -ne 3 ]]; then
-            echo ¡"$mob_name" ha hecho "${!mob_damage}" puntos de daño! > skill_dmg
+            if [[ ( $random_target -ne 1 && shield_f_1 -ne 1 ) || ( $random_target -ne 2 && shield_f_2 -ne 1 )]]; then
+                echo ¡"$mob_name" ha hecho "${!mob_damage}" puntos de daño! > skill_dmg
+            
+            else
+                echo "$mob_name" ha intentado atacar al "$random_target" pero tiene un Escudo Físico.
+            fi
         else
             echo ¡"$mob_name" se ha subido el ataque físico! > skill_dmg
         fi
@@ -21,8 +27,6 @@ mob_turn(){
         echo ¡"$mob_name" no ha podido atacar porque ha sido desarmado! > skill_dmg
         desarmar=0
     fi
-
-    random_target=$(RANDOM%2 + 1)
 
     sed -n 1p skill_text
     sed -n 1p skill_dmg
